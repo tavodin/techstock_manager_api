6bf88d1dba0b4fd4bba0d0b39bd785aca8a2a06f
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2026-05-04 17:54:08 -0300

    CategoryDTO, CategoryRequestDTO

---
+++ src/main/java/io/github/tavodin/techstock_manager/dto/CategoryDTO.java
@@ -0,0 +1,46 @@
+package io.github.tavodin.techstock_manager.dto;
+
+import org.springframework.hateoas.server.core.Relation;
+
+import java.time.LocalDateTime;
+
+@Relation(itemRelation = "category", collectionRelation = "categories")
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/io/github/tavodin/techstock_manager/dto/error/CategoryRequestDTO.java
@@ -0,0 +1,4 @@
+package io.github.tavodin.techstock_manager.dto.error;
+
+public record CategoryRequestDTO(String name) {
+}
